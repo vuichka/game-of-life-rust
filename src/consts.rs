303b093bf9b37342c31498f3fa6c3558d4ba5785
Ian <i.e.aleksandrov@tinkoff.ru>
2023-11-05 01:05:49 +0300

    f

--- src/consts.rs
+++ src/consts.rs
@@ -2,8 +2,8 @@ pub const ROWS: i32 = SCREEN_HEIGHT / CELL_HEIGHT;
 pub const COLS: i32 = SCREEN_WIDTH / CELL_WIDTH;
 pub const SCREEN_HEIGHT: i32 = 900;
 pub const SCREEN_WIDTH: i32 = 1500;
-pub const CELL_WIDTH: i32 = 15;
-pub const CELL_HEIGHT: i32 = 15;
+pub const CELL_WIDTH: i32 = 5;
+pub const CELL_HEIGHT: i32 = 5;
 
 // time speed
 pub const REFRESH_MILLIS: u64 = 50;
